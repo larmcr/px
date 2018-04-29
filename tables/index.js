be941c48a0173d58ffb56066ccded66fd1dd85df
Luis Andres Rojas Matey <larmcr@gmail.com>
2018-04-29 14:45:20 -0600

    Update Bib

--- tables/index.js
+++ tables/index.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const data = fs.readFileSync(`./variants/data.json`);
 const tree = JSON.parse(data);
 
-let refStr = '';
+let bibs = [];
 
 _.forEach(tree, (variants, gene) => {
   _.forEach(variants, (races, variant) => {
@@ -12,8 +12,8 @@ _.forEach(tree, (variants, gene) => {
       _.forEach(phens, (sigs, phen) => {
         _.forEach(sigs, (ids, sig) => {
           _.forEach(ids, (cases, id) => {
+            bibs.push(id);
             const bib = fs.readFileSync(`./references/${id}.bib`, 'utf-8');
-            refStr += bib + '\n';
             const ref = /@.+{(.+),/.exec(bib)[1];
             console.info(ref, `[${cases}]`);
           });
@@ -21,6 +21,13 @@ _.forEach(tree, (variants, gene) => {
       });
     });
   });
+});
+
+let refStr = '';
+
+_.uniq(bibs).forEach((id) => {
+  const bib = fs.readFileSync(`./references/${id}.bib`, 'utf-8');
+  refStr += bib + '\n';
 });
 
 fs.writeFileSync(`./tables/bib.bib`, refStr);
